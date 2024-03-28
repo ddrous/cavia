6a7dbc9eaf76cf675ffc6e9040e293a915b7e9a4
rdes <gb21553@bristol.ac.uk>
2024-03-28 12:32:56 +0000

    FInished selkov on daffy duck

--- regression/arguments.py
+++ regression/arguments.py
@@ -21,13 +21,13 @@ def parse_args():
 
     parser.add_argument('--num_inner_updates', type=int, default=1, help='number of inner-loop updates (during training)')
 
-    ## FOr Selkov !
-    # parser.add_argument('--num_context_params', type=int, default=256, help='number of context parameters (added at first layer)')
-    # parser.add_argument('--num_hidden_layers', type=int, nargs='+', default=[82]*5)
+    # FOr Selkov !
+    parser.add_argument('--num_context_params', type=int, default=256, help='number of context parameters (added at first layer)')
+    parser.add_argument('--num_hidden_layers', type=int, nargs='+', default=[84]*5)
 
     ## For GO
-    parser.add_argument('--num_context_params', type=int, default=1024, help='number of context parameters (added at first layer)')
-    parser.add_argument('--num_hidden_layers', type=int, nargs='+', default=[178]*5)
+    # parser.add_argument('--num_context_params', type=int, default=1024, help='number of context parameters (added at first layer)')
+    # parser.add_argument('--num_hidden_layers', type=int, nargs='+', default=[178]*5)
 
     parser.add_argument('--first_order', action='store_true', default=False, help='run first-order version')
 
